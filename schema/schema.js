6054f66a5b8f112939a743f51067ab4db1412c33
Jonathan Schrade <jonathan.schrade@gmail.com>
2021-02-16 14:51:53 -0600

    Get most of the GET requests set up

--- schema/schema.js
+++ schema/schema.js
@@ -1,21 +1,27 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+const { GraphQLSchema, GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList } = graphql;
 
 // dummy data
 var tasks = [
-    { id: '1', title: 'Complete To-Do', description: 'Follow REACT tutorial to get basis of to-do frontend layout', project: '3'  },
-    { id: '2', title: 'Complete API - GraphQL', description: 'Follow GraphQL tutorial to get basis of GraphQL API point working', project: '1' },
-    { id: '3', title: 'Complete API - REST', description: 'Set up REST API that matches GraphQL functionality', project: '1'  }
+    { id: '1', title: 'Complete To-Do', description: 'Follow REACT tutorial to get basis of to-do frontend layout', projectId: '3', status: 1, labels: ['1','3']  },
+    { id: '2', title: 'Complete API - GraphQL', description: 'Follow GraphQL tutorial to get basis of GraphQL API point working', projectId: '1', status: 1, labels: ['1','4'] },
+    { id: '3', title: 'Complete API - REST', description: 'Set up REST API that matches GraphQL functionality', projectId: '1', status: 1, labels: ['1','3'] }
 ];
 var projects = [
-    { id: '1', name: 'API', user: '1' },
-    { id: '2', name: 'SchradeAdevntures', user: '1' },
-    { id: '3', name: 'To-Do', user: '1' }
+    { id: '1', name: 'API', users: ['1'] },
+    { id: '2', name: 'SchradeAdevntures', users: ['1'] },
+    { id: '3', name: 'To-Do', users: ['1'] }
 ];
 var users = [
     { id: '1' , firstName: 'Jonathan', lastName: 'Schrade', email: 'jonathan.schrade@gmail.com' }
+];
+var labels = [
+    { id: '1', name: 'improvement' },
+    { id: '2', name: 'bug-fix' },
+    { id: '3', name: 'to-do' },
+    { id: '4', name: 'working' }
 ];
 
 const UserType = new GraphQLObjectType({
@@ -24,7 +30,13 @@ const UserType = new GraphQLObjectType({
         id: { type: GraphQLID },
         firstName: { type: GraphQLString },
         lastName: { type: GraphQLString },
-        email: { type: GraphQLString }
+        email: { type: GraphQLString },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return  _.filter(projects, ({users}) => { return users.includes(parent.id) });
+            }
+        }
     })
 });
 
@@ -33,7 +45,18 @@ const ProjectType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        user: { type: UserType }
+        users: { 
+            type: new GraphQLList(UserType),
+            resolve(parent, args){
+                return  _.filter(users, ({id}) => {return parent.users.includes(id)});
+            }
+        },
+        tasks: {
+            type: new GraphQLList(TaskType),
+            resolve(parent, args) {
+                return _.filter(tasks, { projectId: parent.id });
+            }
+        },
     })
 }); 
 
@@ -43,9 +66,52 @@ const TaskType = new GraphQLObjectType({
         id: { type: GraphQLID },
         title: { type: GraphQLString },
         description: { type: GraphQLString },
-        project: { type: ProjectType }
+        project: { 
+            type: ProjectType,
+            resolve(parent, args){
+                return _.find(projects, { id: parent.projectId });
+            }
+        },
+        labels: {
+            type: new GraphQLList(LabelType),
+            resolve(parent, args) {
+                return  _.filter(labels, ({id}) => { return parent.labels.includes(id) });
+            }
+        }
     })
 });
+
+const LabelType = new GraphQLObjectType({
+    name: 'Label',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        tasks: {
+            type: new GraphQLList(TaskType),
+            resolve(parent, args) {
+                return _.filter(tasks, ({labels}) => { return labels.includes(parent.id) })
+            }
+        }
+    })
+});
+
+// const TaskLabelType = new GraphQLObjectType({
+//     name: 'TaskLabel',
+//     fields: () => ({
+//         task: { 
+//             type: TaskType,
+//             resolve(parent, args) {
+//                 return _.find(tasks, { id : parent.taskId });
+//             }
+//         },
+//         label: { 
+//             type: LabelType,
+//             resolve(parent, args) {
+//                 return _.find(labels, { id : parent.labelId });
+//             } 
+//         }
+//     })
+// });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -71,6 +137,37 @@ const RootQuery = new GraphQLObjectType({
                 return _.find(tasks, { id: args.id });
             }
         },
+        label: {
+            type: LabelType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(labels, { id: args.id });
+            }
+        },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args) {
+                return users;
+            }
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        tasks: {
+            type: new GraphQLList(TaskType),
+            resolve(parent, args) {
+                return tasks;
+            }
+        },
+        labels: {
+            type: new GraphQLList(LabelType),
+            resolve(parent, args) {
+                return labels;
+            }
+        }
     }
 });
 
